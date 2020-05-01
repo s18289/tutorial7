02b5758d233d1c8db2c856d0e57c9f29fa9f2fac
s18289 <s18289@pjwstk.edu.pl>
2020-05-01 21:50:23 +0200

    commit

---
+++ tutorial7/Controllers/StudentsController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace tutorial7.Controllers
+{
+    [Route("api/students")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+
+        public IConfiguration Configuration { get; set; }
+
+        public StudentsController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult GetStudents()
+        {
+            return Ok("Secret string");
+        }
+
+        [HttpPost]
+        public IActionResult Login()
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, "1"),
+                new Claim(ClaimTypes.Name, "Den123"),
+                new Claim (ClaimTypes.Role, "admin"),
+                new Claim(ClaimTypes.Role, "student")
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["SecretKey"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken
+            (
+                issuer: "Gakko",
+                audience: "Students",
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(10),
+                signingCredentials: creds
+            ); 
+
+            return Ok(new
+            {
+                accessToken = new JwtSecurityTokenHandler().WriteToken(token),
+                refreshToken = Guid.NewGuid()
+            });
+        }
+
+        [HttpPost("refresh-token/{token}")]
+        public IActionResult RefreshToken(string token)
+        {
+            //check in database if token exists
+            return Ok();
+        }
+    }
+}
---
+++ tutorial7/Handlers/BasicAuthHandler.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace tutorial7.Handlers
+{
+    public class BasicAuthHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+
+        public BasicAuthHandler(
+            IOptionsMonitor<AuthenticationSchemeOptions> options,
+            ILoggerFactory logger,
+            UrlEncoder encoder,
+            ISystemClock clock) : base(options, logger, encoder, clock)
+        {
+
+        }
+
+
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            if (!Request.Headers.ContainsKey("Authorization"))
+            {
+                return AuthenticateResult.Fail("Missing authentication header");
+            }
+
+            var authHeader = AuthenticationHeaderValue.Parse(Request.Headers["Authorization"]);
+            var credentialBytes = Convert.FromBase64String(authHeader.Parameter);
+            var credentials = Encoding.UTF8.GetString(credentialBytes).Split(":");
+
+            if (credentials.Length != 2)
+            {
+                return AuthenticateResult.Fail("Incorrect authentication header");
+            }
+
+            //check the password against database
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, "1"),
+                new Claim(ClaimTypes.Name, "jan123"),
+                new Claim(ClaimTypes.Role, "admin"),
+                new Claim(ClaimTypes.Role, "student")
+            };
+
+            var identity = new ClaimsIdentity(claims, Scheme.Name);
+            var principal = new ClaimsPrincipal(identity);
+            var ticket = new AuthenticationTicket(principal, Scheme.Name);
+
+            return AuthenticateResult.Success(ticket);
+        }
+
+        public static string SecurePassword(string value, string salt)
+        {
+            var valueBytes = KeyDerivation.Pbkdf2(
+                                                    password: value,
+                                                    salt: Encoding.UTF8.GetBytes(salt),
+                                                    prf: KeyDerivationPrf.HMACSHA512,
+                                                    iterationCount: 10000,
+                                                    numBytesRequested: 256 / 8);
+            return Convert.ToBase64String(valueBytes);
+        }
+
+        public static bool Validate(string value, string salt, string hash)
+            => SecurePassword(value, salt) == hash;
+
+        public static string CreateSalt()
+        {
+            byte[] randomBytes = new byte[128 / 8];
+            using (var generator = RandomNumberGenerator.Create())
+            {
+                generator.GetBytes(randomBytes);
+                return Convert.ToBase64String(randomBytes); 
+            }
+        }
+    }
+}
